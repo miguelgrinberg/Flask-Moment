cbcb45255da057d1fb6e64ef688a7b45baf37d9d
javl <mail@jaspervanloenen.com>
2015-10-27 11:55:10 +0100

    Adds shebang to app.py so it can be run using ./app.py

--- example/app.py
+++ example/app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from datetime import datetime
 from flask import Flask, render_template, jsonify
 from flask.ext.moment import Moment
@@ -9,12 +11,12 @@ moment = Moment(app)
 def index():
     now = datetime.utcnow()
     midnight = datetime(now.year, now.month, now.day, 0, 0, 0)
-    epoch = datetime(1970, 1, 1, 0, 0, 0) 
+    epoch = datetime(1970, 1, 1, 0, 0, 0)
     return render_template('index.html', now=now, midnight=midnight, epoch=epoch)
 
 @app.route('/ajax')
 def ajax():
     return jsonify({ 'timestamp': moment.create(datetime.utcnow()).format('LLLL') });
- 
+
 if __name__ == '__main__':
     app.run(debug = True)
