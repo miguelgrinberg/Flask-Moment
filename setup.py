7c38231786a5c8867e7d928df43e3d8fa3ddcbba
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-09-28 19:47:30 -0700

    Release 0.5.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Moment',
-    version='0.5.1',
+    version='0.5.2',
     url='http://github.com/miguelgrinberg/flask-moment/',
     license='MIT',
     author='Miguel Grinberg',
