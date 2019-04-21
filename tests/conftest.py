19c1decfc6d166f960ec9c251552c43f6ad001ab
Leon <leonrichter@fastmail.com>
2019-04-21 17:12:25 +0200

    implements sri + adds unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
 import pytest
-import os.path, sys
-sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 from flask import Flask
 from flask_moment import Moment
 import datetime
