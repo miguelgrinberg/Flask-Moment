0a875738e596957767860a318cfed4d4e6e1ffe7
avycado13 <108358183+avycado13@users.noreply.github.com>
2025-02-21 00:08:22 +0000

    bump default moment version to 2.30.1 and update sri for it

--- src/flask_moment/__init__.py
+++ src/flask_moment/__init__.py
@@ -4,9 +4,8 @@ from markupsafe import Markup
 from flask import current_app
 
 # //cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment-with-locales.min.js
-default_moment_version = '2.29.4'
-default_moment_sri = ('sha512-42PE0rd+wZ2hNXftlM78BSehIGzezNeQuzihiBCvUEB3CVx'
-                      'HvsShF86wBWwQORNxNINlBPuq7rG4WWhNiTVHFg==')
+default_moment_version = '2.30.1'
+default_moment_sri = "sha512-4F1cxYdMiAW98oomSLaygEwmCnIP38pb4Kx70yQYqRwLVCs3DbRumfBq82T08g/4LJ/smbFGFpmeFlQgoDccgg=="
 
 js_code = '''function flask_moment_render(elem) {{
     const timestamp = moment(elem.dataset.timestamp);
